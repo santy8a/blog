1410da9bcb1c5a4920bf695e8570d069170efbaf
Santiago Ochoa <santy@CPX-TETGOI9G9DL.dir.svc.accenture.com>
2020-05-16 09:53:20 +0200

    Cambio de twiter

--- src/components/bio.js
+++ src/components/bio.js
@@ -57,10 +57,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author.name}</strong> {author.summary}
+        Escrito por <strong>{author.name}</strong> {author.summary}
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          Síguenos en twitter
         </a>
       </p>
     </div>
